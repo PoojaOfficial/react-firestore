f3be73f6d87ab1c7bf9b3b61ad03a7ba3951cd55
PoojaOfficial <rajputpooja.praj@gmail.com>
2020-02-04 15:15:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Edit from './components/Edit';
-import Create from './components/Create';
-import Show from './components/Show';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 
@@ -15,9 +12,6 @@ ReactDOM.render(
   <Router>
       <div>
         <Route exact path='/' component={App} />
-        <Route path='/edit/:id' component={Edit} />
-        <Route path='/create' component={Create} />
-        <Route path='/show/:id' component={Show} />
         <Route path='/register' component={Register} />
         <Route path='/dashboard/:id' component={Dashboard} />
       </div>
